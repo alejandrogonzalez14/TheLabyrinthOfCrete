8e55547f0b027515a4ec7495e041d8c0c8df45ae
Alejandro González <115491795+alejandrogonzalez14@users.noreply.github.com>
2025-05-30 10:43:00 +0200

    class upgrades

--- Assets/Scripts/Center/PlayerMovCenter.cs
+++ Assets/Scripts/Center/PlayerMovCenter.cs
@@ -5,20 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovCenter : MonoBehaviour
 {
-    public Quaternion q;
-    public bool manual;
-
     private Vector3 lastPosition;
 
     public float threshold = 3f;
-    private ThrowRock throwRockScript;
+    public ThrowRock throwRockScript;
 
 
     // Start is called before the first frame update
     void Start()
     {
         lastPosition = transform.position;
-        throwRockScript = GetComponent<ThrowRock>();
+        //throwRockScript = GetComponent<ThrowRock>();
     }
 
     // Update is called once per frame
@@ -28,36 +25,14 @@ public class PlayerMovCenter : MonoBehaviour
         lastPosition = transform.position;
     }
 
-    // Setter for position
-    public void SetPosition(Vector3 pos)
-    {
-        transform.position = pos;
-    }
-
-    // Getter for position
-    public Vector3 GetPosition()
-    {
-        return transform.position;
-    }
-
-    // Setter for rotation
-    public void SetRotation(Quaternion rot)
-    {
-        transform.rotation = rot;
-    }
-
-    // Getter for rotation
-    public Quaternion GetRotation()
-    {
-        return transform.rotation;
-    }
-
     private void DetectThrow()
     {
         Vector3 velocity = (transform.position - lastPosition) / Time.deltaTime;
 
-        if (Mathf.Abs(velocity.x) >= threshold)
+        if (velocity.magnitude >= threshold)
         {
+            Debug.Log("TIRO PIEDRA");
+
             if (throwRockScript != null)
             {
                 throwRockScript.throwRocks(); 
